6f2c4c6fed2cf082348eb58809b57d672efe368f
wooodecki <53315413+wooodecki@users.noreply.github.com>
2019-10-30 22:22:53 +0100

    gallery

--- TOJKO/gallery.js
+++ TOJKO/gallery.js
@@ -1,6 +1,19 @@
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myIMG");
+var modalImg = document.getElementById("img01");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
